158095366e069f5d46b7f3f4c535297d1b9aebb0
Pedro Figuera <pedroafiguera@gmail.com>
2014-06-16 13:20:43 -0400

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 ExampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "users/new"
+  get "signup" => "users#new"
+
+  root "static_pages#home"
+  get "help" => "static_pages#help"
+  get "about" => "static_pages#about"
+  get "contact" => "static_pages#contact"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
