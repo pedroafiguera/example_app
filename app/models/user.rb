afcc814095dcd48bc9171d7b7d64c5390ff978fe
Pedro Figuera <pedroafiguera@gmail.com>
2014-06-17 20:48:23 -0400

    Add user micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,6 +10,11 @@ class User < ActiveRecord::Base
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
+  end
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
   end
 
   def User.digest(token)
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ ExampleApp::Application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-
+  resources :microposts, only: [:create, :destroy]
 
 
 
